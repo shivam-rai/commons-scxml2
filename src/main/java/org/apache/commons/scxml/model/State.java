4a46bafdb53d10adf37db9333855c1cb746750bc
bayard <bayard@13f79535-47bb-0310-9956-ffa450edef68>
2007-07-29 03:42:34 +0000

    Moving to TLP
    git-svn-id: http://svn.apache.org/repos/asf/commons/proper/scxml/trunk@560660 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/scxml/model/State.java
@@ -0,0 +1,368 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.scxml.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The class in this SCXML object model that corresponds to the
+ * &lt;state&gt; SCXML element.
+ *
+ */
+public class State extends TransitionTarget {
+
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The Map containing immediate children of this State, keyed by
+     * their IDs. Incompatible with the parallel or invoke property.
+     */
+    private Map children;
+
+    /**
+     * The Parallel child, which defines a set of parallel substates.
+     * May occur 0 or 1 times. Incompatible with the state or invoke property.
+     */
+    private Parallel parallel;
+
+    /**
+     * The Invoke child, which defines an external process that should
+     * be invoked, immediately after the onentry executable content,
+     * and the transitions become candidates after the invoked
+     * process has completed its execution.
+     * May occur 0 or 1 times. Incompatible with the state or parallel
+     * property.
+     */
+    private Invoke invoke;
+
+    /**
+     * Boolean property indicating whether this is a final state or not.
+     * Default value is false . Final states may not have substates or
+     * outgoing transitions.
+     */
+    private boolean isFinal;
+
+    /**
+     * A child which identifies initial state for state machines that
+     * have substates.
+     */
+    private Initial initial;
+
+    /**
+     * A list of outgoing Transitions from this state, by document order.
+     */
+    private List transitions;
+
+    /**
+     * Applies to composite states only. If one of its final children is
+     * active, its parent is marked done. This property is reset upon
+     * re-entry.
+     */
+    private boolean done = false;
+
+    /**
+     * Constructor.
+     */
+    public State() {
+        this.children = new LinkedHashMap();
+        this.transitions = new ArrayList();
+    }
+
+    /**
+     * Is this state a &quot;final&quot; state.
+     *
+     * @return boolean Returns the isFinal.
+     * @deprecated Use {@link #isFinal()} instead
+     */
+    public final boolean getIsFinal() {
+        return isFinal;
+    }
+
+    /**
+     * Is this state a &quot;final&quot; state.
+     *
+     * @return boolean Returns the isFinal.
+     */
+    public final boolean isFinal() {
+        return isFinal;
+    }
+
+    /**
+     * Set whether this is a &quot;final&quot; state.
+     *
+     * @param isFinal
+     *            The isFinal to set.
+     * @deprecated Use {@link #setFinal(boolean)} instead
+     */
+    public final void setIsFinal(final boolean isFinal) {
+        this.isFinal = isFinal;
+    }
+
+    /**
+     * Set whether this is a &quot;final&quot; state.
+     *
+     * @param isFinal
+     *            The isFinal to set.
+     */
+    public final void setFinal(final boolean isFinal) {
+        this.isFinal = isFinal;
+    }
+
+    /**
+     * Get the Parallel child (may be null).
+     *
+     * @return Parallel Returns the parallel.
+     *
+     * @deprecated &lt;parallel&gt; no longer needs an enclosing
+     *             &lt;state&gt; element.
+     */
+    public final Parallel getParallel() {
+        return parallel;
+    }
+
+    /**
+     * Set the Parallel child.
+     *
+     * @param parallel
+     *            The parallel to set.
+     *
+     * @deprecated &lt;parallel&gt; no longer needs an enclosing
+     *             &lt;state&gt; element.
+     */
+    public final void setParallel(final Parallel parallel) {
+        this.parallel = parallel;
+    }
+
+    /**
+     * Get the Invoke child (may be null).
+     *
+     * @return Invoke Returns the invoke.
+     */
+    public final Invoke getInvoke() {
+        return invoke;
+    }
+
+    /**
+     * Set the Invoke child.
+     *
+     * @param invoke
+     *            The invoke to set.
+     */
+    public final void setInvoke(final Invoke invoke) {
+        this.invoke = invoke;
+    }
+
+    /**
+     * Get the initial state.
+     *
+     * @return Initial Returns the initial state.
+     */
+    public final Initial getInitial() {
+        return initial;
+    }
+
+    /**
+     * Set the initial state.
+     *
+     * @param target
+     *            The target to set.
+     */
+    public final void setInitial(final Initial target) {
+        this.initial = target;
+        target.setParent(this);
+    }
+
+    /**
+     * Get the map of all outgoing transitions from this state.
+     *
+     * @return Map Returns the transitions Map.
+     * @deprecated Use {@link #getTransitionsList()} instead
+     */
+    public final Map getTransitions() {
+        Map transitionsMap = new HashMap();
+        for (int i = 0; i < transitions.size(); i++) {
+            Transition transition = (Transition) transitions.get(i);
+            String event = transition.getEvent();
+            if (!transitionsMap.containsKey(event)) {
+                List eventTransitions = new ArrayList();
+                eventTransitions.add(transition);
+                transitionsMap.put(event, eventTransitions);
+            } else {
+                ((List) transitionsMap.get(event)).add(transition);
+            }
+        }
+        return transitionsMap;
+    }
+
+    /**
+     * Get the list of all outgoing transitions from this state, that
+     * will be candidates for being fired on the given event.
+     *
+     * @param event The event
+     * @return List Returns the candidate transitions for given event
+     */
+    public final List getTransitionsList(final String event) {
+        List matchingTransitions = null; // since we returned null upto v0.6
+        for (int i = 0; i < transitions.size(); i++) {
+            Transition t = (Transition) transitions.get(i);
+            if ((event == null && t.getEvent() == null)
+                    || (event != null && event.equals(t.getEvent()))) {
+                if (matchingTransitions == null) {
+                    matchingTransitions = new ArrayList();
+                }
+                matchingTransitions.add(t);
+            }
+        }
+        return matchingTransitions;
+    }
+
+    /**
+     * Add a transition to the map of all outgoing transitions for
+     * this state.
+     *
+     * @param transition
+     *            The transitions to set.
+     */
+    public final void addTransition(final Transition transition) {
+        transitions.add(transition);
+        transition.setParent(this);
+    }
+
+    /**
+     * Get the map of child states (may be empty).
+     *
+     * @return Map Returns the children.
+     */
+    public final Map getChildren() {
+        return children;
+    }
+
+    /**
+     * Add a child state.
+     *
+     * @param state
+     *            a child state
+     *
+     * @deprecated Use {@link #addChild(TransitionTarget)} instead.
+     */
+    public final void addChild(final State state) {
+        this.children.put(state.getId(), state);
+        state.setParent(this);
+    }
+
+    /**
+     * Add a child transition target.
+     *
+     * @param tt
+     *            a child transition target
+     */
+    public final void addChild(final TransitionTarget tt) {
+        this.children.put(tt.getId(), tt);
+        tt.setParent(this);
+    }
+
+    /**
+     * Get the outgoing transitions for this state as a java.util.List.
+     *
+     * @return List Returns the transitions list.
+     */
+    public final List getTransitionsList() {
+        return transitions;
+    }
+
+    /**
+     * Check whether this is a simple (leaf) state (UML terminology).
+     *
+     * @return true if this is a simple state, otherwise false
+     */
+    public final boolean isSimple() {
+        if (parallel == null && children.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check whether this is a composite state (UML terminology).
+     *
+     * @return true if this is a composite state, otherwise false
+     */
+    public final boolean isComposite() {
+        if (parallel == null && children.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks whether it is a region state (directly nested to parallel - UML
+     * terminology).
+     *
+     * @return true if this is a region state, otherwise false
+     * @see Parallel
+     */
+    public final boolean isRegion() {
+        if (getParent() instanceof Parallel) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks whether it is a orthogonal state, that is, it owns a parallel
+     * (UML terminology).
+     *
+     * @return true if this is a orthogonal state, otherwise false
+     * @deprecated &lt;parallel&gt; now represents an orthogonal state, rather
+     *             than denoting that the enclosing state is orthogonal, as
+     *             it did in previous SCXML WDs.
+     */
+    public final boolean isOrthogonal() {
+        if (parallel != null) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * In case this is a parallel state, check if one its final states
+     * is active.
+     *
+     * @return Returns the done.
+     */
+    public final boolean isDone() {
+        return done;
+    }
+
+    /**
+     * Update the done property, which is set if this is a parallel state,
+     * and one its final states is active.
+     *
+     * @param done The done to set.
+     */
+    public final void setDone(final boolean done) {
+        this.done = done;
+    }
+}
+
---
+++ src/test/java/org/apache/commons/scxml/EventDataTest.java
@@ -0,0 +1,166 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.scxml;
+
+import java.net.URL;
+import java.util.Set;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import org.apache.commons.scxml.model.State;
+/**
+ * Unit tests {@link org.apache.commons.scxml.SCXMLExecutor}.
+ * Testing special variable "_eventdata"
+ */
+public class EventDataTest extends TestCase {
+    /**
+     * Construct a new instance of SCXMLExecutorTest with
+     * the specified name
+     */
+    public EventDataTest(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(EventDataTest.class);
+        suite.setName("SCXML Executor Tests, _eventdata special variable");
+        return suite;
+    }
+
+    // Test data
+    private URL eventdata01, eventdata02, eventdata03;
+    private SCXMLExecutor exec;
+
+    /**
+     * Set up instance variables required by this test case.
+     */
+    public void setUp() {
+        eventdata01 = this.getClass().getClassLoader().
+            getResource("org/apache/commons/scxml/env/jexl/eventdata-01.xml");
+        eventdata02 = this.getClass().getClassLoader().
+            getResource("org/apache/commons/scxml/env/jexl/eventdata-02.xml");
+        eventdata03 = this.getClass().getClassLoader().
+            getResource("org/apache/commons/scxml/env/jexl/eventdata-03.xml");
+    }
+
+    /**
+     * Tear down instance variables required by this test case.
+     */
+    public void tearDown() {
+        eventdata01 = eventdata02 = eventdata03 = null;
+    }
+
+    /**
+     * Test the SCXML documents, usage of "_eventdata"
+     */
+    public void testEventdata01Sample() {
+    	exec = SCXMLTestHelper.getExecutor(eventdata01);
+        assertNotNull(exec);
+        try {
+            Set currentStates = exec.getCurrentStatus().getStates();
+            assertEquals(1, currentStates.size());
+            assertEquals("state1", ((State)currentStates.iterator().
+                next()).getId());
+            TriggerEvent te = new TriggerEvent("event.foo",
+                TriggerEvent.SIGNAL_EVENT, new Integer(3));
+            currentStates = SCXMLTestHelper.fireEvent(exec, te);
+            assertEquals(1, currentStates.size());
+            assertEquals("state3", ((State)currentStates.iterator().
+                next()).getId());
+            TriggerEvent[] evts = new TriggerEvent[] { te,
+                new TriggerEvent("event.bar", TriggerEvent.SIGNAL_EVENT,
+                new Integer(6))};
+            currentStates = SCXMLTestHelper.fireEvents(exec, evts);
+            assertEquals(1, currentStates.size());
+            assertEquals("state6", ((State)currentStates.iterator().
+                next()).getId());
+            te = new TriggerEvent("event.baz",
+                TriggerEvent.SIGNAL_EVENT, new Integer(7));
+            currentStates = SCXMLTestHelper.fireEvent(exec, te);
+            assertEquals(1, currentStates.size());
+            assertEquals("state7", ((State)currentStates.iterator().
+                next()).getId());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    public void testEventdata02Sample() {
+    	exec = SCXMLTestHelper.getExecutor(eventdata02);
+        assertNotNull(exec);
+        try {
+            Set currentStates = exec.getCurrentStatus().getStates();
+            assertEquals(1, currentStates.size());
+            assertEquals("state0", ((State)currentStates.iterator().
+                next()).getId());
+            TriggerEvent te1 = new TriggerEvent("connection.alerting",
+                TriggerEvent.SIGNAL_EVENT, "line2");
+            currentStates = SCXMLTestHelper.fireEvent(exec, te1);
+            assertEquals(1, currentStates.size());
+            assertEquals("state2", ((State)currentStates.iterator().
+                next()).getId());
+            TriggerEvent te2 = new TriggerEvent("connection.alerting",
+                TriggerEvent.SIGNAL_EVENT,
+                new ConnectionAlertingPayload(4));
+            currentStates = SCXMLTestHelper.fireEvent(exec, te2);
+            assertEquals(1, currentStates.size());
+            assertEquals("state4", ((State)currentStates.iterator().
+                next()).getId());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    public void testEventdata03Sample() {
+        exec = SCXMLTestHelper.getExecutor(eventdata03);
+        assertNotNull(exec);
+        try {
+            Set currentStates = exec.getCurrentStatus().getStates();
+            assertEquals(1, currentStates.size());
+            assertEquals("ten", ((State)currentStates.iterator().
+                next()).getId());
+            TriggerEvent te = new TriggerEvent("event.foo",
+                TriggerEvent.SIGNAL_EVENT);
+            currentStates = SCXMLTestHelper.fireEvent(exec, te);
+            assertEquals(1, currentStates.size());
+            assertEquals("thirty", ((State)currentStates.iterator().
+                next()).getId());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    public static class ConnectionAlertingPayload {
+        private int line;
+        public ConnectionAlertingPayload(int line) {
+            this.line = line;
+        }
+        public void setLine(int line) {
+            this.line = line;
+        }
+        public int getLine() {
+            return line;
+        }
+    }
+
+    public static void main(String args[]) {
+        TestRunner.run(suite());
+    }
+}
